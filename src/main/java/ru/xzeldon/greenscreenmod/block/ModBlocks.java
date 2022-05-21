85569801190ce941b0e89e9623465ad428ef1670
xzeldon <contact@zeldon.ru>
2022-05-21 08:32:26 +0300

    cosmetic changes
    - add fabric download link to readme
    - replace repo links according to the new repo name

--- src/main/java/ru/xzeldon/greenscreenmod/block/ModBlocks.java
+++ src/main/java/ru/xzeldon/greenscreenmod/block/ModBlocks.java
@@ -8,7 +8,6 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import ru.xzeldon.greenscreenmod.GreenScreenMod;
-import net.minecraft.block.AbstractGlassBlock;
 
 public class ModBlocks {
     public static final Block GREEN_SCREEN_BLOCK = registerBlock("green_screen_block", new GreenScreenBlock(), ItemGroup.MISC);
